a62695a4277e71158797ecf5ec9c10c03ee737e5
nihadaboudou-max <nihad.aboudou@ism.edu.sn>
2025-08-28 01:30:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -283,18 +283,10 @@ if bouton_valider:
                             'Nombre': [st.session_state.stats['billets_authentiques'], 
                                       st.session_state.stats['billets_suspects']]
                         })
-                        # Mise en forme pour le graphique
-                        df_chart = chart_data.set_index('Type')
-
-                        # Palette de couleurs de base
-                        base_colors = ["#10c501", "#EF4444"]
-
-                        # Adapter la liste de couleurs au nombre réel de colonnes
-                        colors = base_colors[:len(df_chart.columns)]
-
+                        
                         # Graphique en barres avec Streamlit
-                        st.bar_chart(df_chart, height=300, color=colors)
-                       
+                        color_base=["#10c501", "#EF4444"]
+                        st.bar_chart(chart_data.set_index('Type'), height=300, color=color_base[:len(chart_data.columns)])
                         st.markdown('</div>', unsafe_allow_html=True)
                     
                     with col_g2:
